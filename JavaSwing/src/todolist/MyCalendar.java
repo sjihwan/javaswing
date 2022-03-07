a5de6a818a49d0ef497a7ec9c56ae0cd9c3ab410
Jihwan Shin <rocrie12@naver.com>
2022-03-07 11:58:53 +0900

    update

--- JavaSwing/src/todolist/MyCalendar.java
+++ JavaSwing/src/todolist/MyCalendar.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,7 +19,7 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class MyCalendar extends JFrame implements ActionListener{
-	DbConnect db = new DbConnect(); //오라클 DB객체 생성
+	DbConnect db=new DbConnect(); //오라클 DB객체 생성
 	Container cp;
 	JLabel lblID,lblPW; //라벨 ID,PW
 	JTextField tfID; //텍스트필드 ID
@@ -39,6 +41,7 @@ public class MyCalendar extends JFrame implements ActionListener{
 	public void initDesign() {
 		this.setLayout(null);
 		
+		//폰트
 		Font a=new Font("카페24 빛나는별", Font.BOLD, 30);
 		
 		//라벨 생성
@@ -75,13 +78,54 @@ public class MyCalendar extends JFrame implements ActionListener{
 		signup.btnSubmit.addActionListener(this);
 	}
 	
+	//로그인 메서드
+	public void login(String inID, String inPW) {
+		String sql="select pw from todo_member where id='"+inID+"'";
+		//오라클DB 연결
+		Connection conn=db.getOracle();
+		//PreparedStatement pstmt=null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		try {
+			stmt=conn.createStatement();
+			//pstmt=conn.prepareStatement(sql);
+			//바인딩
+			//pstmt.setString(1, inID);
+			//SQL문 실행
+			//rs=pstmt.executeQuery(sql);
+			rs=stmt.executeQuery(sql);
+			if(rs.next()) { //ID가 일치하는 것이 있으면
+				//비밀번호 비교
+				if(rs.getString(1).contentEquals(inPW)) {
+					JOptionPane.showMessageDialog(this, "로그인 성공!!");
+				}
+				else {
+					JOptionPane.showMessageDialog(this, "비밀번호가 일치하지 않습니다.");
+				}
+			}
+			else { //ID가 일치하는 것이 없으면
+				JOptionPane.showMessageDialog(this, "해당 아이디가 존재하지 않습니다.");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override //이벤트 메서드
 	public void actionPerformed(ActionEvent e) {
 		Object ob=e.getSource();
-		if(ob==btnSignin) {
+		if(ob==btnSignin) { //로그인 버튼 클릭
+			String id=tfID.getText();
+			String pw=tfPW.getText();
 			
+			if(id.equals("")||pw.equals("")) {
+				JOptionPane.showMessageDialog(this, "아이디와 비밀번호를 정확히 입력해 주세요.");
+			}
+			else {
+				login(id, pw);
+			}
 		}
-		else if(ob==btnSignup) {
+		else if(ob==btnSignup) { //회원가입 버튼 클릭
 			signup.setVisible(true);
 		}
 		else if(ob==signup.btnSubmit) {
@@ -91,24 +135,25 @@ public class MyCalendar extends JFrame implements ActionListener{
 			String birth=signup.tfBirth.getText();
 			
 			String sql="insert into todo_member values(seq_todo.nextval,?,?,?,?)";	
-			//오라클 DB연결
-			Connection conn = db.getOracle();
-			PreparedStatement pstmt = null;
+			//오라클DB 연결
+			Connection conn=db.getOracle();
+			PreparedStatement pstmt=null;
 			
 			//입력을 어느 하나라도 안할 경우
 			if(id.equals("")||pw.equals("")||name.equals("")||birth.equals("")) {
 				JOptionPane.showMessageDialog(this, "정확히 입력해 주세요.");
+				//메시지창 위치 지정했으면 좋겠다
 			}
 			//입력이 바르게 되었을 경우
 			else {
 				try {
-					pstmt = conn.prepareStatement(sql);
+					pstmt=conn.prepareStatement(sql);
 					//바인딩
 					pstmt.setString(1, id);
 					pstmt.setString(2, pw);
 					pstmt.setString(3, name);
 					pstmt.setString(4, birth);
-					//sql문 실행
+					//SQL문 실행
 					pstmt.execute();
 					
 					JOptionPane.showMessageDialog(this, "회원가입이 완료되었습니다!");
